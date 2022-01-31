0c3bdd352767c374c2d831969dc364ebf8db73f8
Ignacio Oscar García <nachog.2000@hotmail.com>
2022-01-31 17:22:06 -0300

    Ej 1 hecho

---
+++ EjerciciosATS/Ej1.py
@@ -0,0 +1,7 @@
+a = float(input("Digita el primer valor: "))
+b = float(input("Digita el segundo valor: "))
+c = float(input("Digita el tercer valor: "))
+
+resultado = (a ** 3 * (b ** 2 - 2 * a * c)) / 2 * b
+
+print(f"El resultado es: {resultado}")
