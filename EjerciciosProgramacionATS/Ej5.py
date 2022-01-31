83cb21d5d94306c03a6c2c28ecb17f624764f654
Ignacio Oscar García <nachog.2000@hotmail.com>
2022-01-31 17:51:27 -0300

    Ejercicio 5 hecho

---
+++ EjerciciosProgramacionATS/Ej5.py
@@ -0,0 +1,6 @@
+totalCompra = float(input("Digita el total de la compra: "))
+descuento = totalCompra * 0.15
+
+precioFinal = totalCompra - descuento
+
+print(f"El precio final con descuento es {precioFinal:.2f}")
